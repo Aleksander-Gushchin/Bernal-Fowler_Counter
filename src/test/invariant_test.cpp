f1f411f25ff41ed36642ea4104bd027aa5382bdc
igush <igush@DESKTOP-3OTTB5S>
2021-04-03 13:00:03 +0300

    added tests to invariant

---
+++ src/test/invariant_test.cpp
@@ -0,0 +1,24 @@
+#include <gtest.h>
+
+#include "invariant.h"
+
+TEST(invariant, basic_init) {
+  ASSERT_NO_THROW(Invariant({ 1, 1, 1 },
+    { {1, 0, 1}, {1, 0, 1},{1, 0, 1} }));
+}
+
+TEST(invariant, can_throw_when_invalid_args) {
+  ASSERT_ANY_THROW(Invariant({ 1, 1, 1, 1 },
+    { {1, 0, 1}, {1, 0, 1},{1, 0, 1} }));
+}
+
+TEST(invariant, get_value) {
+  Invariant inv({ 1, 3, 1 },
+    { {1, 0, 1}, {1, 1, 1},{0, 0, 1} });
+
+  Graph g({ 1, 0, 1 });
+
+  int res = inv.getValue(g);
+
+  EXPECT_EQ(-1, res);
+}
